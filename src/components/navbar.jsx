e4a3da10f2d48034f24ad25565619b5be011497c
Tony Le <tonyle9511@gmail.com>
2022-12-29 17:22:30 -0600

    creates basic layout for website

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+    return(
+        <nav id="nav">
+            <h1>Tony Le</h1>
+            <div>
+                <ul>
+                    <li>about</li>
+                    <li>stack</li>
+                    <li>projects</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
