2534c3517854513dcf63c94afb952d68eab03deb
Tony Le <tonyle9511@gmail.com>
2022-12-31 02:59:17 -0600

    remove react router dom and add home component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { NavBar, Footer, About } from './components';
-import Home from './pages/home'
+import { NavBar, Footer, About, Home } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
+    <main>
       <NavBar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
+      <Home/>
       <About/>
       <Footer/>
-    </BrowserRouter>
+    </main>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import NavBar from "./navbar";
+import Home from "./home";  
 import About from "./about";
 import Footer from "./footer";
 
 export {
   NavBar,
+  Home,
   About,
   Footer
 }
