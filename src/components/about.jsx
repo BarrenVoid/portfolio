d8d7b31b7673199f746a64914183bba860b66387
Tony Le <tonyle9511@gmail.com>
2022-12-31 02:53:09 -0600

    adds about component to page (forgot git add .)

---
+++ src/components/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return(
+    <div className="flex flex-col items-center h-screen w-full">
+      <h1 className="text-2xl sm:text-7xl">About Me</h1>
+      <p>Hello, I'm Tony Le</p>
+    </div>
+  )
+}
+
+export default About
