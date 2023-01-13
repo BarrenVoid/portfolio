89b5e846a83b7ee00eaf9fbfcbd8c20e743eb99e
Tony Le <tonyle9511@gmail.com>
2023-01-13 13:54:01 +0700

    Adds tech stack img and title

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -2,7 +2,80 @@ import React from 'react'
 
 const Stack = () => {
   return (
-    <div>stack</div>
+    <div className="p-12">
+      <h1 className="text-center text-4xl sm:text-7xl pt-12 pb-6">Stack</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 sm:px-48">
+        <div>
+          <img alt="html" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" />
+          <p className="text-center">HTML</p>
+        </div>
+        <div>
+          <img alt="css" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" />
+          <p className="text-center">CSS</p>
+        </div>
+        <div>
+          <img alt="javascript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+          <p className="text-center">Javascript</p>
+        </div>
+        <div>
+          <img alt="php" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg" />
+          <p className="text-center">PHP</p>
+        </div>
+        <div>
+          <img alt="ruby" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg" />
+          <p className="text-center">Ruby</p>
+        </div>
+        <div>
+          <img alt="rails" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg" />
+          <p className="text-center">Rails</p>
+        </div>
+        <div>
+          <img alt="mongodb" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" />
+          <p className="text-center">MongoDB</p>
+        </div>
+        <div>
+          <img alt="express" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+          <p className="text-center">Express</p>
+        </div>
+        <div>
+          <img alt="react" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+          <p className="text-center">React</p>
+        </div>
+        <div>
+          <img alt="nodejs" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" />
+          <p className="text-center">Node.js</p>
+        </div>
+        <div>
+          <img alt="oracle" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg" />
+          <p className="text-center">Oracle SQL</p>
+        </div>
+        <div>
+          <img alt="mysql" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg" />
+          <p className="text-center">MySQL</p>
+        </div>
+        <div>
+          <img alt="tailwindcss" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+          <p className="text-center">TailwindCSS</p>
+        </div>
+        <div>
+          <img alt="bootstrap" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" />
+          <p className="text-center">Bootstrap</p>
+        </div>
+        <div>
+
+        <img alt="materialui" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-plain.svg" />
+          <p className="text-center">Material UI</p>
+        </div>
+        <div>
+          <img alt="git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" />
+          <p className="text-center">Git</p>
+        </div>
+        <div>
+          <img alt="github" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+          <p className="text-center">GitHub</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
