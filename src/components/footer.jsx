178afd66bd80421d9c4d471fe20f8c89daf6d296
Tony Le <tonyle9511@gmail.com>
2023-01-20 10:35:11 +0700

    updates footer information and style

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-      <div id="footer">
-        <p>Built by Tony Le</p>
-        <p>Inprired by ...</p>
+      <div id="footer" className="flex flex-col items-center p-6">
+        <p className="flex">
+          Built by Tony Le with
+          <img className="w-8 h-8 ml-1" alt="react icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+        </p>
+
+        <p>
+          Inprired by
+          <a className="text-[#61DBFB]" href="https://rickyla.ca/"> Ricky La</a>,
+          <a className="text-[#61DBFB]" href="https://www.jarrodservilla.com/"> Jarrod Servilla</a>, and
+          <a className="text-[#61DBFB]" href="https://tamalsen.dev/"> Tamal Sen</a>
+        </p>
       </div>
     )
 }
